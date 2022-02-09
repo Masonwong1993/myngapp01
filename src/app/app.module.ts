274c1e90aed6222618b86568b302276bd8748daa
hjw <masonwong1993@gmail.com>
2022-02-08 21:54:41 -0600

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginC } from './loginc';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,LoginC
   ],
   imports: [
     BrowserModule
---
+++ src/app/loginc.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'loginc',
+    templateUrl: './loginc.html'
+})
+export class LoginC{
+
+}
